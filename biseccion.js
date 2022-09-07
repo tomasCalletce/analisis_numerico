56c87f385f77019a992ff9b6727b02852da5dbf0
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2022-09-07 09:33:18 -0500

    all working 🚀

--- biseccion.js
+++ biseccion.js
@@ -1,7 +1,6 @@
-// hay un error 
-
 const biseccion = (x0,x1,tolerancia,maxIter) => {
     const fx = (x)=> x**3 -7.51*x**2 + 18.4239*x - 14.8331
+
     const med = (x,y) => (x+y)/2
     let fx0 = fx(x0)
     let fx1 = fx(x1)
@@ -10,9 +9,9 @@ const biseccion = (x0,x1,tolerancia,maxIter) => {
     if(fx1 == 0) return x1 + " es raiz"
     if(fx1*fx0 < 0){
         let xmed = med(x0,x1)
-        let fmed = fx(med)
+        let fmed = fx(xmed)
         let err = tolerancia + 1
-        while(err > tolerancia && fx1 != 0 && fx0 != 0 && fmed != 0 && maxIter != 0){
+        while(err > tolerancia && fmed != 0 && maxIter != 0){
             if(fx0*fmed < 0){
                 x1 = xmed
                 fx1 = fmed
@@ -22,7 +21,7 @@ const biseccion = (x0,x1,tolerancia,maxIter) => {
             }
             let aux = xmed
             xmed = med(x0,x1)
-            fmed = fx(med)
+            fmed = fx(xmed)
             err = Math.abs(xmed - aux)
             maxIter--
         }
@@ -35,4 +34,4 @@ const biseccion = (x0,x1,tolerancia,maxIter) => {
     
 }
 
-console.log(biseccion(3,3.5,10**-5,1000))
+console.log(biseccion(3.2099999999997575,3.2109999999997574,10**-5,1000))
--- busquedasIncrementales.js
+++ busquedasIncrementales.js
@@ -1,26 +1,32 @@
 
 
 const busquedasIncrementales = (xInicial,delta,maxIter) => {
+    const arr = []
     const fx = (x)=> x**3 -7.51*x**2 + 18.4239*x - 14.8331
-    
-    let fx0 = fx(xInicial)
-    if(fx0 == 0) return "x0 es raiz"
-    let x0 = xInicial
-    let x1 = xInicial + delta
-    let fx1 = fx(x1)
+    const grado = 3
+    let iter = 0;
+    let a = xInicial;
+    while(iter < grado){
+        let fx0 = fx(a)
+        if(fx0 == 0) return "x0 es raiz"
+        let x0 = a
+        let x1 = a + delta
+        let fx1 = fx(x1)
 
-    while(fx(x1)*fx(x0) > 0 && maxIter != 0){
-        x0 = x1
-        fx0 = fx(x0)
-        x1 = x1 + delta
-        fx1 = fx(x1)
-        maxIter--
+        while(fx(x1)*fx(x0) > 0 && maxIter != 0){
+            x0 = x1
+            fx0 = fx(x0)
+            x1 = x1 + delta
+            fx1 = fx(x1)
+            maxIter--
+        }
+
+        if(fx1 == 0) arr.push(x0 + " es raiz")
+        if(fx(x1)*fx(x0) < 0) arr.push("raiz entre " + x0 + " y " + x1)
+        iter++
+        a = x1
     }
-
-    if(fx1 == 0) return "x0 es raiz"
-    if(fx(x1)*fx(x0) < 0) return "raiz entre " + x0 + " y " + x1;
-    return "error maxIter"
-    
+    return arr
 }
 
-console.log(busquedasIncrementales(0,.5,10000))
+console.log(busquedasIncrementales(0,.001,10000))
